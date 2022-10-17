0f87f7daa3b1eba3dbf60410d24a9badfcd9ed94
BrookeBryski <brookeabryski@gmail.com>
2022-10-17 15:53:54 -0700

    configured h2 database and jpa orm

---
+++ src/main/java/com/example/ProjectPurpleCow/ProjectPurpleCowApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectPurpleCow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPurpleCowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPurpleCowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectPurpleCow/ProjectPurpleCowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectPurpleCow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPurpleCowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
